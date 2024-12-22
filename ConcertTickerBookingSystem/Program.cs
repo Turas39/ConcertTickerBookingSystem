a9986767068db69fa9448e8b13e6727759fdaad6
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-22 19:47:20 +0100

    Inicjalizacja projektu z klasą Concert Dodano klasę Concert reprezentującą koncert z podstawowymi właściwościami: name, date, location, availableSeats

--- ConcertTickerBookingSystem/Program.cs
+++ ConcertTickerBookingSystem/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Concert
+{
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public int AvailableSeats { get; set; }
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        AvailableSeats = availableSeats;
+    }
+}
 
 internal class Program
 {
